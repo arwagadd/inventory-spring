883865f96a49e6d2ccab1abb5d2aca9a53d9da9b
Arwa <arwa_wail@hotmail.com>
2023-09-26 16:55:12 +0300

    Request Type Statistics and Pagination

--- src/main/java/com/example/inventory/repository/ItemHistoryRepo.java
+++ src/main/java/com/example/inventory/repository/ItemHistoryRepo.java
@@ -2,6 +2,7 @@ package com.example.inventory.repository;
 
 import com.example.inventory.model.ItemHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.domain.Pageable;
@@ -12,5 +13,8 @@ public interface ItemHistoryRepo extends JpaRepository<ItemHistory,Long> {
     List<ItemHistory> findAllByItemIdAndUserId(Long itemId, Long userId);
     List<ItemHistory> findAllByItemName(String item);
     List<ItemHistory> findAllByUserName(String user);
+
+    @Query("select i from ItemHistory i ORDER BY i.id")
+
     List<ItemHistory> findItemHistoriesByPage(Pageable pageable);
 }
